8cf9beb8f8b98243364f2e233808960d048778df
mumtazhadladytechie <amumtazha@gmail.com>
2023-05-03 11:59:44 +0100

    cart.html cart.js data.js images/ index.html main.js style.css

---
+++ data.js
@@ -0,0 +1,72 @@
+let shopItemsData = [ 
+
+    {
+        id: "breateair01",
+        name: "Casual Shirt",
+        price: 45,
+        desc: "Aerowear casual men's shirt.",
+        img: "images/shirt2.jpg"
+    },
+
+    {
+
+        id: "ableoften",
+        name: "Laptop Backpack",
+        price: 100,
+        desc: "Unisex Laptop Backpack Grey.",
+        img: "images/backpack.webp"
+    },
+
+    {
+        id: "breatheablealways",
+        name: "Vaccum Flask",
+        price: 25,
+        desc: "Vaccum Insulated Temperature Flask.",
+        img: "images/vaccum.webp"
+    },
+
+    {
+        id: "fdbjhjjgh",
+        name: "Fashion Sweater",
+        price: 300,
+        desc: "Unisex Multicolored Fashion Sweater.",
+        img: "images/fashion-sweater.jpg"
+    },   
+
+    {
+        id: "breffdggd",
+        name: "Men's Shirt",
+        price: 200,
+        desc: "Casual Men's Shirt",
+        img: "images/shirt.jpg"
+
+    },
+
+    {
+        id: "fahhhhj",
+        name: "Back Hat",
+        price: 50,
+        desc: "Unisex Back Hat",
+        img: "images/back-hat.jpg"
+
+    },
+
+    {
+        id: "vkbjkjjf",
+        name: "Wireless Earbuds",
+        price: 100,
+        desc: "Black Wireless Earbuds",
+        img: "images/wireless-airpod.jpg"
+    },
+
+    {
+        id: "hjdhskjd",
+        name: "Men's Timberland",
+        price: 150,
+        desc: "Mustard Men's Timberland",
+        img: "images/timberland.webp"
+
+    }
+
+
+];
